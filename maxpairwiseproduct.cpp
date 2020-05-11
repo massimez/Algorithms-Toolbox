73350d99e595d859ee6aaed22f57854850c6bada
Massi <mez.massinissa@gmail.com>
2020-05-11 11:34:07 +0700

    Update maxpairwiseproduct.cpp

--- maxpairwiseproduct.cpp
+++ maxpairwiseproduct.cpp
@@ -6,26 +6,28 @@
 long long MaxPairwiseProduct(const std::vector<long long> &numbers)
 {
     long long max_product = 0;
-    long long max1 =numbers[0];
-    long long max2 =numbers[0];
+    long long max1 = numbers[0];
+    long long max2 = numbers[0];
     int n = numbers.size();
-    int positionmax1 = 0; 
-    
+    int positionmax1 = 0;
+
     for (int first = 0; first < n; ++first)
     {
-        if (numbers[first]>max1) {
-          max1 = numbers[first];
-          positionmax1 = first;
+        if (numbers[first] > max1)
+        {
+            max1 = numbers[first];
+            positionmax1 = first;
         }
     }
     for (int second = 0; second < n; ++second)
+    {
+        if (numbers[second] > max2 && positionmax1 != second)
         {
-            if (numbers[second]>max2 && positionmax1 != second){
-              max2=numbers[second+1];
-            }
+            max2 = numbers[second + 1];
         }
-    std::cout<<"MAx1:"<<max1<<"max2:"<<max2<<std::endl;    
-    max_product = max1*max2;
+    }
+    std::cout << "MAx1:" << max1 << "max2:" << max2 << std::endl;
+    max_product = max1 * max2;
     return max_product;
 }
 
@@ -41,9 +43,9 @@ int main()
 
     std::cout << MaxPairwiseProduct(numbers) << std::endl;
 
-//clock_t end = clock();
-//double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-//std::cout<<"Time:"<<time_spent;
+    //clock_t end = clock();
+    //double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    //std::cout<<"Time:"<<time_spent;
 
     return 0;
 }
